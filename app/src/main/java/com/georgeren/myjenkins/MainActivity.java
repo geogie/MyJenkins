f8bef37272a676337dbd717c5f556cae8e452169
geogie <geogie.ren@firstleap.cn>
2017-12-19 16:11:47 +0800

    同步下

--- app/src/main/java/com/georgeren/myjenkins/MainActivity.java
+++ app/src/main/java/com/georgeren/myjenkins/MainActivity.java
@@ -2,6 +2,7 @@ package com.georgeren.myjenkins;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tvText = (TextView) findViewById(R.id.tv_test);
+        tvText.setText("线上:"+BuildConfig.ONLINE+" 渠道:"+BuildConfig.CHANNEL);
     }
 }
